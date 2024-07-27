02cfba622fcb75ac23418b4c3e662c077208863b
Enock <enocksm@outlook.com>
2024-07-27 18:49:31 -0300

    commit com quase todo projeto pronto, so adicioanr algumar funcionalidades

---
+++ js/script.js
@@ -0,0 +1,102 @@
+
+// 1 - REQUEST COM AXIOS
+let data = []; // Declaração global da variável data
+
+let currentPage = 0;
+const pageSize = 72;
+
+const getData = async (page = 0, query = '') => {
+    try {
+        let url = `http://localhost:8080/api/examples?page=${page}&size=${pageSize}`;
+        if (query) {
+            url = `http://localhost:8080/api/examples/search?query=${query}&page=${page}&size=${pageSize}`;
+        }
+        const response = await axios.get(url);
+        console.log(response);
+        const data = response.data.content; // Guardando os dados globalmente
+        printData(data); // Chamada para imprimir dados iniciais
+
+        // Atualizar totalPages e currentPage
+        totalPages = response.data.totalPages;
+        currentPage = response.data.number;
+        
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+document.querySelector("#next").addEventListener("click", () => {
+    if (currentPage < totalPages - 1) {
+        currentPage++;
+        getData(currentPage);
+    }
+});
+
+document.querySelector("#previous").addEventListener("click", () => {
+    if (currentPage > 0) {
+        currentPage--;
+        getData(currentPage);
+    }
+});
+
+// 2 - Imprimindo dados na tela
+
+const container = document.querySelector("#user-container");
+
+const printData = (dataToPrint) => {
+    container.innerHTML = ""; // Limpa o container antes de imprimir novos dados
+
+    dataToPrint.forEach((user) => {
+        const div = document.createElement("div");
+
+        const imgElement = document.createElement("img");
+        imgElement.src = user.position;
+        imgElement.alt = user.name;
+        div.appendChild(imgElement);
+
+        const nameElement = document.createElement("h2");
+        nameElement.textContent = user.name;
+        div.appendChild(nameElement);
+
+        const ppg = document.createElement("h3");
+        ppg.textContent = user.ppg;
+        div.appendChild(ppg); // colocar para concatenar
+        // o R$ junto ao dado que vier do banco, o R$ fica no html
+
+
+        container.appendChild(div);
+    });
+}
+
+getData(); // Carrega os dados na inicialização
+
+// Form method POST
+
+// const form = document.querySelector("#post-form");
+// const titleInput = document.querySelector("#title");
+// const bodyInput = document.querySelector("#body");
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+
+//     axios.post("https://jsonplaceholder.typicode.com/posts", {
+//         title: titleInput.value,
+//         body: bodyInput.value,
+//     })
+// })
+
+// Script para barra de pesquisa
+
+const searchInput = document.querySelector("#searchInput");
+
+searchInput.addEventListener("keyup", (e) => {
+    const search = e.target.value.toLowerCase();
+    console.log('Valor de busca:', search);
+    console.log('Dados disponíveis:', data);
+    const filteredUsers = data.filter(user => {
+        return user.name.toLowerCase().includes(search);
+    });
+    getData(0, search); // Chama getData com a query de busca
+    console.log('Usuários filtrados:', filteredUsers);
+    printData(filteredUsers); // Chama printData com os dados filtrados
+});
